8e5cf1d523dd686b1c040dae537b6cbabfbc363a
Mega Pangastuti <pangastuti.mega@gmail.com>
2026-05-07 15:36:06 +0700

    [ADD] add login page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginPage from "./components/pages/LoginPage/LoginPage";
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+}
+
+export default App;
